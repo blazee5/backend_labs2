5a2eed3a01b68bb7c9136a9102762507ffaff9fa
blazee5 <kutsenko.vladislav2005@gmail.com>
2023-01-26 09:17:18 +0600

    ЛПР 8 Упр. 1

--- inc/cookie.inc.php
+++ inc/cookie.inc.php
@@ -1,1 +1,12 @@
-<?
+<?php
+$visitCounter = 0;
+$visitCounter = $_COOKIE['visitCounter'] ?? 0;
+$visitCounter++;
+$lastVisit = "";
+$date = date_create();
+setcookie('visitCounter', $visitCounter);
+setcookie('lastVisit', date_timestamp_get($date));
+
+echo $visitCounter . "<br>";
+echo $_COOKIE['lastVisit'];
+?>
