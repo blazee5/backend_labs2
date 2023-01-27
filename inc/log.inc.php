8ce480151192424a6c663e5e6c92bab327c5ae5e
blazee5 <kutsenko.vladislav2005@gmail.com>
2023-01-27 10:44:51 +0600

    ЛПР 9

--- inc/log.inc.php
+++ inc/log.inc.php
@@ -1,1 +1,8 @@
-<?
+<?php
+$dt = date("d:m:Y H:i:s");
+$page = $_SERVER['REQUEST_URI'];
+$ref = $_SERVER['HTTP_REFERER'];
+$path = "$dt|$page|$ref\n";
+file_put_contents('log/path.log', $path, FILE_APPEND);
+
+?>
--- inc/view-log.inc.php
+++ inc/view-log.inc.php
@@ -1,1 +1,8 @@
-<?
+<?php
+$lines = file("log/path.log");
+echo "<ul>";
+foreach ($lines as $link) {
+    echo "<li>$link</li>";
+}
+echo "</ul>";
+?>
