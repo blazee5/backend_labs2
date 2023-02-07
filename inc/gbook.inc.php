eaed82d42345fe777565aa17514b5c0b16fd9340
Vladislav Kutsenko <109062318+blazee5@users.noreply.github.com>
2023-02-07 22:03:36 +0600

    ЛПР 12 Упр. 4, 5

--- inc/gbook.inc.php
+++ inc/gbook.inc.php
@@ -1,48 +1,55 @@
 <?php
-if (isset($_POST)) {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $msg = htmlspecialchars($_POST['msg']);
+/* Основные настройки */
+$DB_HOST = "localhost";
+$DB_LOGIN = "root";
+$DB_PASSWORD = "";
+$DB_NAME = "gbook";
+$con = mysqli_connect($DB_HOST, $DB_LOGIN, $DB_PASSWORD, $DB_NAME);
+/* Основные настройки */
+
+/* Сохранение записи в БД */
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $msg = $_POST['msg'];
+  $query = "INSERT INTO msgs(name, email, msg) VALUES (?, ?, ?)";
+  $stmt = mysqli_prepare($con, $query);
+  mysqli_stmt_bind_param($stmt, "sss", $name, $email, $msg);
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_close($stmt);
+  if (!$stmt) {
+    echo 'Ошибка: '
+      . mysqli_errno($con)
+      . ':'
+      . mysqli_error($con);
+  }
 }
-    /* Основные настройки */
-    $DB_HOST = "localhost";
-    $DB_LOGIN = "root";
-    $DB_PASSWORD = "";
-    $DB_NAME = "gbook";
-    $con = mysqli_connect($DB_HOST, $DB_LOGIN, $DB_PASSWORD, $DB_NAME);
-    /* Основные настройки */
+/* Сохранение записи в БД */
 
-    /* Сохранение записи в БД */
-    $query = "INSERT INTO msgs(name, email, msg) VALUES (?, ?, ?)";
-    $stmt = mysqli_prepare($con, $query);
-    mysqli_stmt_bind_param($stmt, "sss", $name, $email, $msg);
-    mysqli_stmt_execute($stmt);
-    mysqli_stmt_close($stmt);
-    $result = mysqli_query($con, 'SELECT * FROM msgs');
-    if (!$result) {
-        echo 'Ошибка: '
-            . mysqli_errno($con)
-            . ':'
-            . mysqli_error($con);
-    }
-    $row = mysqli_fetch_assoc($result);
-    /* Сохранение записи в БД */
+/* Удаление записи из БД */
+if (isset($_GET['del'])) {
+  $del = $_GET['del'];
+  $delete = "DELETE FROM msgs WHERE id = (?)";
+  $stmt = mysqli_prepare($con, $delete);
+  mysqli_stmt_bind_param($stmt, "i", $del);
+  mysqli_stmt_execute($stmt);
+  mysqli_stmt_close($stmt);
+  header("Location: $_SERVER[HTTP_REFERER]");
+}
 
-    /* Удаление записи из БД */
-
-    /* Удаление записи из БД */
+/* Удаление записи из БД */
 ?>
-<h3>Оставьте запись в нашей Гостевой книге</h3>
+  <h3>Оставьте запись в нашей Гостевой книге</h3>
 
-<form method="post" action="">
-Имя: <br /><input type="text" name="name" /><br />
-Email: <br /><input type="text" name="email" /><br />
-Сообщение: <br /><textarea name="msg"></textarea><br />
-<br />
+  <form method="post" action="">
+    Имя: <br/><input type="text" name="name"/><br/>
+    Email: <br/><input type="text" name="email"/><br/>
+    Сообщение: <br/><textarea name="msg"></textarea><br/>
+    <br/>
 
-<input type="submit" value="Отправить!" />
+    <input type="submit" value="Отправить!"/>
 
-</form>
+  </form>
 <?php
 /* Вывод записей из БД */
 $select = mysqli_query($con, "SELECT id, name, email, msg, UNIX_TIMESTAMP(datetime) as dt FROM msgs ORDER BY id DESC");
@@ -51,7 +58,8 @@ $count = mysqli_num_rows($select);
 mysqli_close($con);
 echo "<p>" . "Всего записей в гостевой книге: " . $count . "</p>";
 foreach ($res as $item) {
-    echo "<p>" . "<a href='$item[2]'>$item[1] </a>" . $item[3] . $item[4] . " в " . $item[4] . " написал<br />$item[3]" . "</p>" . "<p align='right'>" . "<a href='http://localhost/backend_labs2/index.php?id=gbook&del=$item[0]'>Удалить</a>" . "</p>";
+  echo "<p>" . "<a href='$item[2]'>$item[1] </a>" . " " . gmdate("d-m-Y", $item[4]) . " в " . gmdate("H:i", $item[4]) . " написал<br />$item[3]" . "</p>" . "<p align='right'>" . "<a href='index.php?id=gbook&del=$item[0]'>Удалить</a>" . "</p>";
 }
 /* Вывод записей из БД */
+
 ?>
--- inc/log.inc.php
+++ inc/log.inc.php
@@ -1,7 +1,7 @@
 <?php
 $dt = date("d:m:Y H:i:s");
 $page = $_SERVER['REQUEST_URI'];
-$ref = $_SERVER['HTTP_REFERER'];
+$ref = $_SERVER['HTTP_REFERER'] ?? '';
 $path = "$dt|$page|$ref\n";
 file_put_contents('log/path.log', $path, FILE_APPEND);
 
