d4830613332cd50045e7cabbf5e5da958054532c
blazee5 <vladislavkutsenko742@gmail.com>
2023-02-23 11:50:05 +0600

    ЛПР 17 Часть 1 Задание 3 Упр. 1, 2

--- ЛПР 17 ООП/index.php
+++ ЛПР 17 ООП/index.php
@@ -18,9 +18,25 @@ class User {
     }
 }
 
+class SuperUser extends User {
+    public $role;
+    function __construct($name, $login, $password, $role)
+    {
+        parent::__construct($name, $login, $password);
+        $this->role = $role;
+    }
+    function showInfo()
+    {
+        echo "Роль: $this->role"."<br>";
+        parent::showInfo();
+    }
+}
+
 $user1 = New User("имя", "логин", "пароль");
 $user1->showInfo();
 $user2 = New User("имя2", "логин2", "пароль2");
 $user2->showInfo();
 $user3 = New User("им3", "логин3", "пароль3");
 $user3->showInfo();
+$superuser = new SuperUser("Супер имя", "Супер логин", "Супер пароль", "Супер пользователь");
+$superuser->showInfo();
